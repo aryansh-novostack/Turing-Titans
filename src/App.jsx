0b2c99be027cd3ad94ffd5d29fcac16ed08144ac
AryanshGautam <159770996+AryanshGautam@users.noreply.github.com>
2025-02-14 15:48:24 +0530

    Signup done

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Login from './components/login'
+import { Signup } from './components/signup'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-      <Login />
-      </div>
+      {/* <Login /> */}
+      <Signup />
     </>
   )
 }
